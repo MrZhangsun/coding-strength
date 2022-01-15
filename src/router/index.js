b46f391d404e2536d2a07607993fcb2230ada625
zhangsun.jiankun <mr_zhangsun@outlook.com>
2022-01-15 21:02:38 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import VueRouter from 'vue-router'
 import Login from '../components/system/Login.vue'
 import Home from '../components/system/Home.vue'
 import Welcome from '../components/system/Welcome.vue'
+import Repository from '../components/coding/Repository.vue'
+import Reporter from '../components/coding/Reporter.vue'
+import Analysis from '../components/coding/Analysis.vue'
+import Logger from '../components/coding/Logger.vue'
+import Menu from '../components/system/Menu.vue'
+import Account from '../components/system/Account.vue'
 import User from '../components/system/User.vue'
 
 Vue.use(VueRouter)
@@ -10,27 +16,71 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Auto redirect to login if not login',
+    name: '系统入口',
     redirect: '/login'
   },
   {
-    name: 'Login',
+    name: '登录',
     path: '/login',
     component: Login
   },
   {
-    name: 'Home',
+    name: '主页',
     path: '/home',
     component: Home,
     redirect: '/welcome',
     children: [
       {
-        name: 'Welcome',
+        name: '首页',
         path: '/welcome',
         component: Welcome
+      }
+    ]
+  },
+  {
+    name: '代码统计',
+    path: '/coding',
+    component: Home,
+    children: [
+      {
+        name: '仓库配置',
+        path: 'repository',
+        component: Repository
       },
       {
-        name: 'User',
+        name: '统计报表',
+        path: 'reporter',
+        component: Reporter
+      },
+      {
+        name: '统计分析',
+        path: 'analysis',
+        component: Analysis
+      },
+      {
+        name: '统计日志',
+        path: 'logger',
+        component: Logger
+      }
+    ]
+  },
+  {
+    name: '系统管理',
+    path: '/system',
+    component: Home,
+    children: [
+      {
+        name: '菜单管理',
+        path: 'menu',
+        component: Menu
+      },
+      {
+        name: '账号管理',
+        path: 'account',
+        component: Account
+      },
+      {
+        name: '用户管理',
         path: 'user',
         component: User
       }
