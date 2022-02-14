bdaf47bc474ed13c4854bea2670dcbd657b8ba83
zhangsun.jiankun <mr_zhangsun@outlook.com>
2022-02-14 15:43:45 +0800

    修改生产IP地址为外网地址

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -4,7 +4,7 @@ switch (process.env.NODE_ENV) {
     baseURL = 'http://localhost:10110/api/'
     break
   case 'production':
-    baseURL = 'http://172.19.225.151:10110/api/'
+    baseURL = 'http://47.101.42.90:10110/api/'
     break
 }
 
