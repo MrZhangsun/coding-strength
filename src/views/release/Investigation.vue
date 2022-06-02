c6ac147cbfbde30c3b1351a1c76eb15cc6732dd1
zhangsun.jiankun <mr_zhangsun@outlook.com>
2022-06-02 10:16:40 +0800

    修复新增指标项标题长度校验

--- src/views/release/Investigation.vue
+++ src/views/release/Investigation.vue
@@ -818,7 +818,7 @@ export default {
       addIndicatorFormRules: {
         name: [
           { required: true, message: '请输入调研问题名称', trigger: 'blur' },
-          { min: 3, max: 20, message: '请输入3~20个字符', trigger: 'blur' }
+          { min: 3, max: 60, message: '请输入3~60个字符', trigger: 'blur' }
         ],
         label: [
           { required: true, message: '请输入调研问题', trigger: 'blur' },
